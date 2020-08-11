4b921f1348ec8a4f9f4453209f5c4634c49c64b5
wyswwill <wyswill4290>
2020-08-11 10:20:32 +0800

    新题目 + 文件夹修改

---
+++ leetcode/editor/cn/[21]合并两个有序链表.js
@@ -0,0 +1,56 @@
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+//
+// 
+//
+// 示例： 
+//
+// 输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4
+// 
+// Related Topics 链表 
+// 👍 1197 👎 0
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val);
+  this.next = (next === undefined ? null : next);
+}
+
+const l1 = new ListNode(1, new ListNode(2, new ListNode(4, new ListNode(5)))),
+  l2 = new ListNode(1, new ListNode(3));
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+var mergeTwoLists = function(l1, l2) {
+  const prehead = new ListNode(-1);
+
+  let prev = prehead;
+  while (l1 != null && l2 != null) {
+    if (l1.val <= l2.val) {
+      prev.next = l1;
+      l1 = l1.next;
+    } else {
+      prev.next = l2;
+      l2 = l2.next;
+    }
+    prev = prev.next;
+  }
+
+  // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+  prev.next = l1 === null ? l2 : l1;
+
+  return prehead.next;
+};
+
+
+//leetcode submit region end(Prohibit modification and deletion)
+mergeTwoLists(l1, l2);
