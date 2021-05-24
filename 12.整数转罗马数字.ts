adf7d59a466bb74240f666ea660ce507bc708b82
wyswill <1151373571@qq.com>
2021-05-24 15:05:32 +0800

    feat

---
+++ 12.整数转罗马数字.ts
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=12 lang=typescript
+ *
+ * [12] 整数转罗马数字
+ */
+
+// @lc code=start
+function intToRoman(num: number): string {
+  let str = '';
+  const map = {
+    1: 'I',
+    5: 'V',
+    10: 'X',
+    50: 'L',
+    100: 'C',
+    500: 'D',
+    1000: 'M',
+  };
+
+
+
+  
+  return str;
+}
+// @lc code=end
